46046c18743bde115baa232687785d230066713c
Vachek-hooves <vachek0602@gmail.com>
2024-09-19 19:02:19 +0300

    update data

---
+++ data/cityIconData.js
@@ -0,0 +1,9 @@
+export const CITY_ICON = [
+  {id: 'c1', icon: require('../assets/cityIcon/image1.png'), isLocked: false},
+  {id: 'c2', icon: require('../assets/cityIcon/image2.png'), isLocked: false},
+  {id: 'c3', icon: require('../assets/cityIcon/image3.png'), isLocked: false},
+  {id: 'c4', icon: require('../assets/cityIcon/image4.png'), isLocked: false},
+  {id: 'c5', icon: require('../assets/cityIcon/image5.png'), isLocked: false},
+  {id: 'c6', icon: require('../assets/cityIcon/image6.png'), isLocked: false},
+  {id: 'c7', icon: require('../assets/cityIcon/image7.png'), isLocked: false},
+];
