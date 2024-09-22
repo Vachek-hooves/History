ad11f77550bed7787fc727afbabecabfac864326
Vachek-hooves <vachek0602@gmail.com>
2024-09-22 19:08:06 +0300

    updated

---
+++ screen/LibraryArticles.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const LibraryArticles = () => {
+  return (
+    <View>
+      <Text>LibraryArticles</Text>
+    </View>
+  );
+};
+
+export default LibraryArticles;
+
+const styles = StyleSheet.create({});
